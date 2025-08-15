c826aa93b571c815a2cda7fb9ffcfd67dfc89f55
kevin579 <125067771+kevin579@users.noreply.github.com>
2025-08-15 17:24:10 -0400

    Setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import "reset-css/reset.css"
+import "./assets/styles/global.scss"
+ 
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
